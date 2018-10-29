2fd17a923edfdea85f7545d4d0f3219013b107c7
KROEGER <moritz.kroeger@ilt.fraunhofer.de>
2018-10-29 13:06:51 +0100

    script for presentation mode added

--- run_presentation.bat
+++ run_presentation.bat
@@ -1,2 +1,3 @@
 call activate jupyter
-jupyter nbconvert Jupyter\ Microtraining.ipynb --to slides --post serve
+jupyter nbconvert Jupyter\ Microtraining.ipynb --to slides --post serve --reveal-prefix ./reveal.js
+pause
